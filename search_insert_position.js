f18f6cc3f973faefd5a12d5e33e7ea199fe6b06d
Brendan Woo <woo.brendan@gmail.com>
2022-04-12 17:05:06 -0400

    complete standard method for question. no binary

--- search_insert_position.js
+++ search_insert_position.js
@@ -1,7 +1,13 @@
 const searchInsert = (nums, target) => {
-
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) return i; 
+  }
+  nums.push(target);
+  nums.sort()
+  return nums.indexOf(target)
 }
+  
 
-const numbers = [1, 2, 3, 5, 6];
-const tar = 5;
+const numbers = [1, 3, 5, 6];
+const tar = 7;
 console.log(searchInsert(numbers, tar));
