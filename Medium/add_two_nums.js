529730aa3c98a21a0c65cf0983a1cd7f981443ff
Brendan Woo <woo.brendan@gmail.com>
2022-05-31 13:20:10 -0400

    complete logic using linked list

--- Medium/add_two_nums.js
+++ Medium/add_two_nums.js
@@ -1,14 +1,34 @@
-const l1 = [9,9,9,9,9,9,9];
-const l2 = [9,9,9,9];
+const addTwoNums = (l1, l2) => {
+  let head = new ListNode(0);
+  let node = head;
+  let carry = 0;
+  //travese linked lists until both lists have reached null or end
+  while(l1 || l2) {
+    let l1Value = l1 ? l1.val : 0;
+    let l2Value = l2 ? l2.val : 0;
 
-const addTwoNums = (list1, list2) => {
-  const first = Number(list1.reverse().join(''));
-  const second = Number(list2.reverse().join(''))
-  const combine = String(first + second).split('').reverse()
-  const numified = combine.map(num => (
-    Number(num)
-  ))
-  return numified;
+    let sum = l1Value + l2Value + carry;
+    carry = 0;
+    let newValue = sum
+
+    if (sum > 9) {
+      newValue = sum % 10;
+      carry = 1;
+    }
+    node.next = new ListNode(newValue);
+    node = node.next
+
+    if (l1) {
+      l1 = l1.next
+    }
+
+    if (l2) {
+      l2 = l2.next
+    }
+  }
+  //if the last node has carry over beyond length of list
+  if (carry) {
+    node.next = new ListNode(carry)
+  }
+  return head.next
 }
-
-console.log(addTwoNums(l1, l2))
